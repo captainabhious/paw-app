21e676a2cf00f9f93ccb6cb38d59e205749d246d
Aldo Ayala <ayalaa63@gmail.com>
2018-04-23 16:13:54 -0700

    petObj view started

--- paw-app/ViewController.swift
+++ paw-app/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var petName: UITextField!
+    @IBOutlet weak var petBirthday: UITextField!
+    @IBOutlet weak var petBreed: UITextField!
+
+    @IBOutlet weak var petWeight: UITextField!
+    @IBOutlet weak var petRegistration: UITextField!
+    @IBOutlet weak var petDateAccuired: UITextField!
+    @IBOutlet weak var petMicroChipNum: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addPetObj(_ sender: Any) {
+        
+    }
+    
 }
 
